f1dbfa74afda0c6131ef738d61ce13dda23a8556
amangeldievkuu <amangeldievkuu@gmail.com>
2024-03-18 21:58:48 +0800

    safari view

---
+++ Apple-Frameworks/SafariView.swift
@@ -0,0 +1,21 @@
+//
+//  SafariView.swift
+//  Apple-Frameworks
+//
+//  Created by Kutman Amangeldiev on 2024/3/18.
+//
+
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    var url: URL
+  
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
+        
+    }
+}
